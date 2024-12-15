55926fbb393be9baec934256c4fb77e2ad218538
Manas Ranjan Adha <manas865873@gmail.com>
2024-12-15 12:41:36 +0530

    first commit

---
+++ src/main/java/com/codewithmanas/demoapi/DemoapiApplication.java
@@ -0,0 +1,14 @@
+package com.codewithmanas.demoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoapiApplication.class, args);
+		System.out.println("App is running...");
+	}
+
+}
